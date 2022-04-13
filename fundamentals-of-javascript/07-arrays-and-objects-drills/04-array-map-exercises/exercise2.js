07bf50419df063cab0f7256b59240c856a4de65a
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-13 11:08:27 +0800

    Update exercise2.js

--- fundamentals-of-javascript/07-arrays-and-objects-drills/04-array-map-exercises/exercise2.js
+++ fundamentals-of-javascript/07-arrays-and-objects-drills/04-array-map-exercises/exercise2.js
@@ -2,13 +2,17 @@
 // Tips: You might want to Google on how to capitalize a string
 
 const capitalizeListOfNames = (arr) => {
-  // Add your code here
-
+    // Add your code here
+    arr.splice(0, 2, "john", " jacob")
+    return arr.map(cap => cap.toUpperCase()) // + cap.slice(1, 1)) => look for what is this
 }
-  
+
 // DO NOT EDIT CODE BELOW
 // Test Cases
 console.log(capitalizeListOfNames(["peter", "DARREN", "timCook", "sundar pichai"]));
 
 // Expected Output
 // ["JOHN", "JACOB", "TIMCOOK", "SUNDAR PICHAI"]
+
+
+// need to replace peter = john, darren = jacob
