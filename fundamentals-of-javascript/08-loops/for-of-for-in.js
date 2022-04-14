cd200f33d94f7c0e8e0c78d32d109f518631ad56
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-15 02:09:28 +0800

    update for-of-for-in.js

--- fundamentals-of-javascript/08-loops/for-of-for-in.js
+++ fundamentals-of-javascript/08-loops/for-of-for-in.js
@@ -15,9 +15,9 @@ for (var i of arr) {
 let list = [4, 5, 6];
 
 for (let i in list) {
-   console.log(i); // "0", "1", "2",
+   console.log(i); // "0", "1", "2" => gives index
 }
 
 for (let i of list) {
-   console.log(i); // "4", "5", "6"
+   console.log(i); // "4", "5", "6" => gives elements
 }
