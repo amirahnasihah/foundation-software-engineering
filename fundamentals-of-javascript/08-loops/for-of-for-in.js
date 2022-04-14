4f49cb431181b27e7c9a7a78269898ee21fdfdf5
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-15 02:07:51 +0800

    update for-of-for-in.js

--- fundamentals-of-javascript/08-loops/for-of-for-in.js
+++ fundamentals-of-javascript/08-loops/for-of-for-in.js
@@ -6,7 +6,18 @@ for (var i in arr) {
 }
     
 for (var i of arr) {
-  console.log(i); // logs "3", "5", "7"
+  console.log(i + "\"); // logs "3", "5", "7"
   // it doesn't log "3", "5", "7", "hello"
 }
 
+///////////////////
+
+let list = [4, 5, 6];
+
+for (let i in list) {
+   console.log(i); // "0", "1", "2",
+}
+
+for (let i of list) {
+   console.log(i); // "4", "5", "6"
+}
