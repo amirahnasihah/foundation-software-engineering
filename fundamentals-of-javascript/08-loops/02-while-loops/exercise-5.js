cfa1a89d304649b121b7fa028d404ec61e6b5a39
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-15 15:49:53 +0800

    update exercise-5.js

--- fundamentals-of-javascript/08-loops/02-while-loops/exercise-5.js
+++ fundamentals-of-javascript/08-loops/02-while-loops/exercise-5.js
@@ -6,11 +6,12 @@ const input = 10
 // Add your code here
 
 let result = input;
-let i = input + 0;
+let i = input - 1;
 while (i > 0 && i < result){
 	result *= i;
 	i--;
 }
+
 
 console.log(result)
 
