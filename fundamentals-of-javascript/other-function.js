1bd2c5869a936da643ec4b38125c4398083d4f47
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-16 17:20:44 +0800

    update other-function.js

--- fundamentals-of-javascript/other-function.js
+++ fundamentals-of-javascript/other-function.js
@@ -16,18 +16,18 @@ guessStart = () => {
 	guess = prompt("enter a number from 1 - 100: ");
 	
 	if (guess < 0 || guess > 100) { // 1-100
-		alert("between 1-100 only! ") } else { 
+		alert("between 1-100 only! ")
+		
+	} else { 
 		if (guess == randomNumber) {
-		foundCorrectNumber = true;
-		alert("Won. The number is " + randomNumber);
+			foundCorrectNumber = true;
+			alert("Won. The number is " + randomNumber);
 			
 		} else if (guess < randomNumber) {
-		alert("too low")
+			alert("too low")
 			
 		} else if (guess > randomNumber) {
-		alert("too high")
-			
-		} else { // other than number
+			alert("too high") else { // other than number
 		alert("no letter, number only! ")
 		}
 	}
