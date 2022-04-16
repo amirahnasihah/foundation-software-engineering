bb41ed11bd994e820cea9ef2a3f401b3f8d9cb47
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-17 04:19:15 +0800

    update tictactoe-1.js

--- fundamentals-of-javascript/10-wrap-up-project/tictactoe-1.js
+++ fundamentals-of-javascript/10-wrap-up-project/tictactoe-1.js
@@ -8,7 +8,7 @@ Players 'X' and 'O' take turn inputing their position on the command line using 
 */
 
 // importing user import library
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({ sigint: true });
 const assert = require('assert');
 
 // The board object used to save the current status of a gameplay
@@ -26,12 +26,20 @@ let board = {
 
 // TODO: update the gameboard with the user input
 function markBoard(position, mark) {
-
+    board[position] = mark.toUpperCase();
 }
 
 // TODO: print the game board as described at the top of this code skeleton
 // Will not be tested in Part 1
 function printBoard() {
+    console.log('\n' +
+        ' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + '\n' +
+        ' ---------\n' +
+        ' ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + '\n' +
+        ' ---------\n' +
+        ' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + '\n'
+    );
+
 
 }
 
@@ -42,26 +50,47 @@ function printBoard() {
 // another case is that the position is already occupied
 // position is an input String
 function validateMove(position) {
-
+    return (printBoard(position) && board[position] === ' ')
 }
 
 // TODO: list out all the combinations of winning, you will neeed this
 // one of the winning combinations is already done for you
 let winCombinations = [
     [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
 ];
 
 // TODO: implement a logic to check if the previous winner just win
 // This method should return with true or false
 function checkWin(player) {
-
+    let i, j, count;
+    for (i = 0; j < winCombinations[i].length; j++) {
+        if (board[winCombinations[i][j]] === player) {
+            count++;
+        }
+        if (count === 3) {
+            return true;
+        }
+    }
+    return false;
 }
 
 // TODO: implement a function to check if the game board is already full
 // For tic-tac-toe, tie bascially means the whole board is already occupied
 // This function should return with boolean
 function checkFull() {
-
+    for (var i = 1; i <= Object.keys(board).length; i++) {
+        if (board[i] === ' ') {
+            return false;
+        }
+    }
+    return true;
 }
 
 
@@ -83,9 +112,15 @@ assert.strictEqual(validateMove('5'), true, "validateMove() didn't work as expec
 assert.strictEqual(validateMove('9'), true, "validateMove() didn't work as expected with input : 9");
 
 let tsetBoard = {
-    1: 'X', 2: 'O', 3: 'X',
-    4: 'O', 5: 'X', 6: 'O',
-    7: ' ', 8: ' ', 9: ' '
+    1: 'X',
+    2: 'O',
+    3: 'X',
+    4: 'O',
+    5: 'X',
+    6: 'O',
+    7: ' ',
+    8: ' ',
+    9: ' '
 };
 
 // Test markBoard()
@@ -109,49 +144,85 @@ assert.strictEqual(checkWin('X'), true, "checkWin() didn't work as expected with
 
 
 board = {
-    1: 'X', 2: ' ', 3: ' ',
-    4: 'O', 5: 'X', 6: ' ',
-    7: 'O', 8: ' ', 9: 'X'
+    1: 'X',
+    2: ' ',
+    3: ' ',
+    4: 'O',
+    5: 'X',
+    6: ' ',
+    7: 'O',
+    8: ' ',
+    9: 'X'
 }
 assert.strictEqual(checkWin('X'), true, "checkWin() didn't work as expected with input : 'X'");
 assert.strictEqual(checkWin('O'), false, "checkWin() didn't work as expected with input : 'O'");
 
 board = {
-    1: 'O', 2: ' ', 3: ' ',
-    4: 'X', 5: 'O', 6: ' ',
-    7: 'X', 8: 'X', 9: 'O'
+    1: 'O',
+    2: ' ',
+    3: ' ',
+    4: 'X',
+    5: 'O',
+    6: ' ',
+    7: 'X',
+    8: 'X',
+    9: 'O'
 }
 assert.strictEqual(checkWin('O'), true, "checkWin() didn't work as expected with input : 'O'");
 assert.strictEqual(checkWin('X'), false, "checkWin() didn't work as expected with input : 'X'");
 
 board = {
-    1: 'X', 2: 'O', 3: 'O',
-    4: 'X', 5: ' ', 6: ' ',
-    7: 'X', 8: ' ', 9: ' '
+    1: 'X',
+    2: 'O',
+    3: 'O',
+    4: 'X',
+    5: ' ',
+    6: ' ',
+    7: 'X',
+    8: ' ',
+    9: ' '
 }
 assert.strictEqual(checkWin('X'), true, "checkWin() didn't work as expected with input : 'X'");
 assert.strictEqual(checkWin('O'), false, "checkWin() didn't work as expected with input : 'O'");
 
 board = {
-    1: 'X', 2: 'O', 3: 'X',
-    4: 'X', 5: 'O', 6: ' ',
-    7: ' ', 8: 'O', 9: ' '
+    1: 'X',
+    2: 'O',
+    3: 'X',
+    4: 'X',
+    5: 'O',
+    6: ' ',
+    7: ' ',
+    8: 'O',
+    9: ' '
 }
 assert.strictEqual(checkWin('O'), true, "checkWin() didn't work as expected with input : 'O'");
 assert.strictEqual(checkWin('X'), false, "checkWin() didn't work as expected with input : 'X'");
 
 board = {
-    1: 'X', 2: 'X', 3: 'X',
-    4: 'O', 5: 'O', 6: ' ',
-    7: ' ', 8: ' ', 9: ' '
+    1: 'X',
+    2: 'X',
+    3: 'X',
+    4: 'O',
+    5: 'O',
+    6: ' ',
+    7: ' ',
+    8: ' ',
+    9: ' '
 }
 assert.strictEqual(checkWin('X'), true, "checkWin() didn't work as expected with input : 'X'");
 assert.strictEqual(checkWin('O'), false, "checkWin() didn't work as expected with input : 'O'");
 
 board = {
-    1: 'X', 2: 'X', 3: ' ',
-    4: 'O', 5: 'O', 6: 'O',
-    7: 'X', 8: ' ', 9: ' '
+    1: 'X',
+    2: 'X',
+    3: ' ',
+    4: 'O',
+    5: 'O',
+    6: 'O',
+    7: 'X',
+    8: ' ',
+    9: ' '
 }
 assert.strictEqual(checkWin('O'), true, "checkWin() didn't work as expected with input : 'O'");
 assert.strictEqual(checkWin('X'), false, "checkWin() didn't work as expected with input : 'X'");
@@ -160,9 +231,15 @@ assert.strictEqual(checkWin('X'), false, "checkWin() didn't work as expected wit
 // Test checkFull()
 assert.strictEqual(checkFull(), false, "checkFull() didn't work as expected");
 board = {
-    1: 'O', 2: 'X', 3: 'O',
-    4: 'O', 5: 'X', 6: 'X',
-    7: 'X', 8: 'O', 9: 'X'
+    1: 'O',
+    2: 'X',
+    3: 'O',
+    4: 'O',
+    5: 'X',
+    6: 'X',
+    7: 'X',
+    8: 'O',
+    9: 'X'
 }
 assert.strictEqual(checkFull(), true, "checkFull() didn't work as expected");
 
