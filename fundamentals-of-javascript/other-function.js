e9255a2b9bc904b305c27c587f5d85964f99416c
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-16 19:04:40 +0800

    update other-function.js

--- fundamentals-of-javascript/other-function.js
+++ fundamentals-of-javascript/other-function.js
@@ -2,19 +2,16 @@
 // const prompt = require('prompt-sync')({ sigint: true });
 
 // Random number from 1 - 100
-// let randomNumber = Math.floor(Math.random() * 100) + 1; 
+let randomNumber = Math.floor(Math.random() * 100) + 1; 
 
 // This variable is used to determine if the app should continue prompting the user for input
 let foundCorrectNumber = false;
 
 
-
+let guess;
 
 
 guessStart = () => {
-	let randomNumber = Math.floor(Math.random() * 100) + 1;
-	let guess;
-
 	
 	
 	while(!foundCorrectNumber) {
