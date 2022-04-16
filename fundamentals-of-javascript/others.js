e18e9b960c5276a0831ec23cbc3206e70876691a
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-16 16:27:52 +0800

    update others.js

---
+++ fundamentals-of-javascript/others.js
@@ -0,0 +1,8 @@
+
+// const prompt = require('prompt-sync')({ sigint: true });
+
+// Random number from 1 - 100
+let randonNumber = Math.floor(Math.random() * 100) + 1; 
+
+// This variable is used to determine if the app should continue prompting the user for input
+let foundCorrectNumber = false;
