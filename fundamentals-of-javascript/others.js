8a5fd28f468998ff111a317420a1146a0b427096
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-16 16:42:19 +0800

    update others.js

--- fundamentals-of-javascript/others.js
+++ fundamentals-of-javascript/others.js
@@ -7,3 +7,11 @@ let randomNumber = Math.floor(Math.random() * 100) + 1;
 // This variable is used to determine if the app should continue prompting the user for input
 let foundCorrectNumber = false;
 
+
+
+
+while(!foundCorrectNumber) {
+	guess = prompt("enter a number from 1 - 100: ");
+	
+	if guess
+}
