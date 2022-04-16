955e884c6ccd295762eaad0c431db2793aec7a63
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-17 01:42:09 +0800

    update part0tttlc.js

---
+++ fundamentals-of-javascript/10-wrap-up-project/part0tttlc.js
@@ -0,0 +1,87 @@
+/*  A simple Tic-Tac-Toe game
+Players 'X' and 'O' take turn inputing their position on the command line using numbers 1-9
+1 | 2 | 3
+---------
+4 | 5 | 6
+---------
+7 | 8 | 9
+*/
+
+// importing user import library
+// missed ({sigint: true});
+const prompt = require('prompt-sync')({sigint: true});
+
+let board = {
+    1: ' ', 2: ' ', 3: ' ',
+    4: ' ', 5: ' ', 6: ' ',
+    7: ' ', 8: ' ', 9: ' '
+};
+
+// TODO: update the gameboard with the user input
+function markBoard(position, mark) {
+
+}
+
+// TODO: print the game board as described at the top of this code skeleton
+function printBoard() {
+
+}
+
+
+// TODO: check for wrong input, this function should return true or false.
+// true denoting that the user input is correct
+// you will need to check for wrong input (user is entering invalid position) or position is out of bound
+// another case is that the position is already occupied
+function validateMove(position) {
+
+}
+
+// TODO: list out all the combinations of winning, you will neeed this
+// one of the winning combinations is already done for you
+let winCombinations = [
+    [1, 2, 3],
+];
+
+// TODO: implement a logic to check if the previous winner just win
+// This method should return with true or false
+function checkWin(player) {
+
+}
+
+// TODO: implement a function to check if the game board is already full
+// For tic-tac-toe, tie bascially means the whole board is already occupied
+// This function should return with boolean
+function checkFull() {
+
+}
+
+// *****************************************************
+// Copy all your code/fucntions in Part 1 to above lines
+// (Without Test Cases)
+// *****************************************************
+
+
+// TODO: the main part of the program
+// This part should handle prompting the users to put in their next step, checking for winning or tie, etc
+function playTurn(player) {
+
+}
+
+// entry point of the whole program
+console.log('Game started: \n\n' +
+    ' 1 | 2 | 3 \n' +
+    ' --------- \n' +
+    ' 4 | 5 | 6 \n' +
+    ' --------- \n' +
+    ' 7 | 8 | 9 \n');
+
+let winnerIdentified = false
+let currentTurnPlayer = 'X'
+
+while (!winnerIdentified){
+    playTurn(currentTurnPlayer);
+    // feel free to add logic here if needed, e.g. announcing winner or tie
+}
+
+
+// Bonus Point: Implement the feature for the user to restart the game after a tie or game over
