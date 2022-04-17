531be7b6b15eaf044bfe7e16c5262009729c6406
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-18 03:21:48 +0800

    Add files via upload

---
+++ fundamentals-of-javascript/10-wrap-up-project/testting/scratchtictac.js
@@ -0,0 +1,42 @@
+/*  A simple Tic-Tac-Toe game
+Players 'X' and 'O' take turn inputing their position on the command line using numbers 1-9
+1 | 2 | 3
+---------
+4 | 5 | 6
+---------
+7 | 8 | 9
+*/
+
+// importing user import library
+const prompt = require('prompt-sync')({ sigint: true });
+const assert = require('assert');
+
+// The board object used to save the current status of a gameplay
+let board = {
+    1: ' ',
+    2: ' ',
+    3: ' ',
+    4: ' ',
+    5: ' ',
+    6: ' ',
+    7: ' ',
+    8: ' ',
+    9: ' '
+};
+
+// TODO: update the gameboard with the user input
+function markBoard(position, mark) {
+    board[position] = mark.toUpperCase();
+}
+
+// TODO: print the game board as described at the top of this code skeleton
+// Will not be tested in Part 1
+function printBoard() {
+    console.log('\n' +
+        ' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + '\n' +
+        ' ---------\n' +
+        ' ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + '\n' +
+        ' ---------\n' +
+        ' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + '\n'
+    );
+}
