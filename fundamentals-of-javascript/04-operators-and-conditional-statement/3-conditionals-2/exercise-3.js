ef6b824eb9404f67104ab2f87e9875c403d7529b
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-05-29 12:02:47 +0800

    Update exercise-3.js

--- fundamentals-of-javascript/04-operators-and-conditional-statement/3-conditionals-2/exercise-3.js
+++ fundamentals-of-javascript/04-operators-and-conditional-statement/3-conditionals-2/exercise-3.js
@@ -1,12 +1,12 @@
 /*
-*  Conditionals
-*  ---------------------------------
-*  Write a conditional statement that checks if a student has passed
-*  - if the mark is 80 or higher then the grade is "A"
-*  - if the mark is lower than 80 and greater than 60 then the grade is "B"
-*  - if the mark is 60 or lower but no lower than 50 then the grade is "C"
-*  - Otherwise the grade is "F"
-*/
+ *  Conditionals
+ *  ---------------------------------
+ *  Write a conditional statement that checks if a student has passed
+ *  - if the mark is 80 or higher then the grade is "A"
+ *  - if the mark is lower than 80 and greater than 60 then the grade is "B"
+ *  - if the mark is 60 or lower but no lower than 50 then the grade is "C"
+ *  - Otherwise the grade is "F"
+ */
 
 
 grade = 49
@@ -16,14 +16,14 @@ if (grade > 80) {
     console.log(grade + ": A");
 } else if (grade < 80 && grade > 60) {
     console.log(grade + ": B");
-} else if (grade == 60 || grade > 50) {
+} else if (grade <= 60 && grade >= 50) {
     console.log(grade + ": C");
 } else {
     console.log(grade + ": F")
 }
 
 /*
-*  EXPECTED RESULT
-*  ---------------
-*  49: F
-*/
+ *  EXPECTED RESULT
+ *  ---------------
+ *  49: F
+ */
