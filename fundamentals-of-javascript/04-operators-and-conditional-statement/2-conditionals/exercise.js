86bf5ec8b79f592ba9a524fc40f91a9c1b94651a
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-05-29 12:01:04 +0800

    Update exercise.js

--- fundamentals-of-javascript/04-operators-and-conditional-statement/2-conditionals/exercise.js
+++ fundamentals-of-javascript/04-operators-and-conditional-statement/2-conditionals/exercise.js
@@ -1,23 +1,23 @@
 /*
-*  Conditionals
-*  ---------------------------------
-*  Add an if statement to check Daniel's role in a CYF class.
-*  If Daniel is a mentor, print out "Hi, I'm Daniel, I'm a mentor."
-*  If Daniel is a student, print out "Hi, I'm Daniel, I'm a student."
-*/
+ *  Conditionals
+ *  ---------------------------------
+ *  Add an if statement to check Daniel's role in a CYF class.
+ *  If Daniel is a mentor, print out "Hi, I'm Daniel, I'm a mentor."
+ *  If Daniel is a student, print out "Hi, I'm Daniel, I'm a student."
+ */
 
 name = "Daniel"
 danielsRole = "mentor"
 
 // Add your Code Here
-if (danielsRole = true) {
+if (danielsRole === "mentor") {
     console.log("Hi, I'm " + name + ", I'm a mentor.")
-} if (danielsRole = false) {
+} else {
     console.log("Hi, I'm " + name + ", I'm a student.")
 }
 
 /*
-*EXPECTED RESULT
-*---------------
-*Hi, I'm Daniel, I'm a mentor.
-*/
+ *EXPECTED RESULT
+ *---------------
+ *Hi, I'm Daniel, I'm a mentor.
+ */
